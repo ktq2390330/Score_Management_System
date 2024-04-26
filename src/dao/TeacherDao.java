a6bb008422ea518f4178cdf3dd25771010ddff9d
ktq2390330 <ktq2390330@stu.o-hara.ac.jp>
2024-04-26 12:19:08 +0900

    create TeacherDao.filter method

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import bean.School;
 import bean.Teacher;
 
 public class TeacherDao extends Dao {
@@ -87,4 +90,69 @@ public class TeacherDao extends Dao {
             sqle.printStackTrace();
         }
     }
+
+    private List<Teacher> postFilter(ResultSet rSet, School school) throws Exception {
+	    List<Teacher> list = new ArrayList<>();
+	    try {
+	        while (rSet.next()) {
+	            Teacher teacher = new Teacher();
+	            teacher.setId("");
+	            teacher.setName(rSet.getString("name"));
+	            teacher.setPassword("");
+	            teacher.setAuthenticated(false);
+	            teacher.setSchool(school);
+	            list.add(teacher);
+	        }
+	    } catch (SQLException | NullPointerException e) {
+	        e.printStackTrace();
+	        // エラーログの出力を行う場合、適切に処理を行います
+	    } finally {
+	        if (rSet != null) {
+	            try {
+	                rSet.close();
+	            } catch (SQLException sqle) {
+	                throw sqle;
+	            }
+	        }
+	    }
+	    return list;
+	}
+
+    public List<Teacher> filter(School school) throws Exception {
+	    List<Teacher> list = new ArrayList<>();
+	    Connection connection = getConnection();
+	    PreparedStatement statement = null;
+	    ResultSet rSet = null;
+	    try {
+	        statement = connection.prepareStatement("select name from teacher where school_cd=?");
+	        statement.setString(1, school.getCd());
+	        rSet = statement.executeQuery();
+	        list = postFilter(rSet, school);
+	    } catch (Exception e) {
+	        throw e;
+	    } finally {
+	        if (rSet != null) {
+	            try {
+	                rSet.close();
+	            } catch (SQLException sqle) {
+	                throw sqle;
+	            }
+	        }
+	        if (statement != null) {
+	            try {
+	                statement.close();
+	            } catch (SQLException sqle) {
+	                throw sqle;
+	            }
+	        }
+	        if (connection != null) {
+	            try {
+	                connection.close();
+	            } catch (SQLException sqle) {
+	                throw sqle;
+	            }
+	        }
+	    }
+	    return list;
+	}
 }
